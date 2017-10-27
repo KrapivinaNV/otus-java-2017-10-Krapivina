1ea318fd06fc0957592ce9897102957e6f90d746
Nataly <krapivina.nv@gmail.com>
2017-10-27 21:57:55 +0300

    [HW02][R] object size calculator after review

--- hw02/src/main/java/otus/ObjectSizeCalculator.java
+++ hw02/src/main/java/otus/ObjectSizeCalculator.java
@@ -5,8 +5,8 @@ public class ObjectSizeCalculator extends SizeCalculator {
     private static final int COUNT_OBJECTS = 1;
 
     @Override
-    public void populateObjects() {
-        Object object = new Object();
+    public Object populateObjects() {
+        return new Object();
     }
 
     @Override
--- hw02/src/main/java/otus/SizeCalculator.java
+++ hw02/src/main/java/otus/SizeCalculator.java
@@ -2,19 +2,20 @@ package otus;
 
 public abstract class SizeCalculator {
 
-    private static final int COUNT_ITERATE = 2000;
+    private static final int COUNT_ITERATE = 20000;
     private static final Runtime RUNTIME = Runtime.getRuntime();
+
+    private final Object[] array = new Object[COUNT_ITERATE];
 
     long calculateSize() {
         long result = 0;
-
         for (int i = 0; i < COUNT_ITERATE; i++) {
             RUNTIME.gc();
 
             long memoryAllocatedBefore = getMemoryAllocated();
+            array[i] = populateObjects();
 
-            populateObjects();
-
+            RUNTIME.gc();
             long memoryAllocatedAfter = getMemoryAllocated();
             result += (memoryAllocatedAfter - memoryAllocatedBefore);
         }
@@ -25,7 +26,7 @@ public abstract class SizeCalculator {
         return RUNTIME.totalMemory() - RUNTIME.freeMemory();
     }
 
-    public abstract void populateObjects();
+    public abstract Object populateObjects();
 
     public abstract int getCount();
 }
