06338c3229b8076c73fe38b79d002c2600f77f3d
Nataly <krapivina.nv@gmail.com>
2017-12-29 15:42:15 +0300

    [HW09][F] My ORM

--- hw09/src/main/java/otus/myorm/MainHW09.java
+++ hw09/src/main/java/otus/myorm/MainHW09.java
@@ -9,19 +9,25 @@ public class MainHW09 {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
         Server server = null;
-
         try {
-            server = Server.createWebServer().start(); // http://localhost:8082/
+            server = Server.createWebServer().start(); // web console http://localhost:8082/
 
-            Executor.save(new UserDataSet(1, "User1", 27));
-            Executor.save(new UserDataSet(2, "User2", 31));
+            UserDataSet user1 = new UserDataSet("User1", 27);
+            UserDataSet user2 = new UserDataSet("User2", 31);
+            UserDataSet user3 = new UserDataSet("User3", 35);
+            UserDataSet user4 = new UserDataSet("User4", 15);
 
-            System.out.println(Executor.load(1, UserDataSet.class).toString());
-            System.out.println(Executor.load(2, UserDataSet.class).toString());
+            Executor.save(user1);
+            Executor.save(user2);
+            Executor.save(user3);
+            Executor.save(user4);
+
+           System.out.println(Executor.load(user1.getId(), UserDataSet.class).toString());
+           System.out.println(Executor.load(user3.getId(), UserDataSet.class).toString());
 
         } finally {
             if (server != null) {
-                server.shutdown();
+                server.stop();
             }
         }
     }
